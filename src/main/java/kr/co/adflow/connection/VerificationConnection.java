87ec213c7789031cd2a6524ca0669f0f0f9a96cc
echan82 <echan1020@gmail.com>
2013-07-10 17:41:14 +0900

    HttpClient Async Sample code

---
+++ src/main/java/kr/co/adflow/connection/VerificationConnection.java
@@ -0,0 +1,64 @@
+package kr.co.adflow.connection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class VerificationConnection {
+
+	public String verificationCheck() {
+
+		long start = System.currentTimeMillis();
+		HttpURLConnection connection = null;
+		OutputStreamWriter wr = null;
+		BufferedReader rd = null;
+		StringBuilder sb = null;
+		String line = null;
+
+		URL serverAddress = null;
+
+		try {
+			serverAddress = new URL("http://127.0.0.1:3000/v1/verificationuri");
+
+			connection = null;
+
+			connection = (HttpURLConnection) serverAddress.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setDoOutput(true);
+			connection.setReadTimeout(10000);
+			connection.connect();
+
+			rd = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			sb = new StringBuilder();
+
+			while ((line = rd.readLine()) != null) {
+				sb.append(line);
+			}
+
+			System.out.println(sb.toString());
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (ProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+
+			connection.disconnect();
+			rd = null;
+			wr = null;
+			connection = null;
+		}
+		System.out.println("VerificationConnection elapsedTime : "
+				+ (System.currentTimeMillis() - start) + " ms ");
+		return sb.toString();
+
+	}
+}
