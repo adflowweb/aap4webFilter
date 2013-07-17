49b90146ada77abc893775c6f1024c3949bbb1e0
echan82 <echan1020@gmail.com>
2013-07-17 16:32:54 +0900

    return commit

--- src/main/java/kr/co/adflow/connection/VirtualBrowserConnection.java
+++ src/main/java/kr/co/adflow/connection/VirtualBrowserConnection.java
@@ -18,10 +18,12 @@ public class VirtualBrowserConnection {
 
 	private static FilterProperites filterProperites = FilterProperites
 			.getInstance();
-	private static final String APP4SERVERIP = filterProperites.read("aap4ServerIp");
-	private static final String VITUALPAGEURI=filterProperites.read("vitualPageUri");
-	private static final String MKDIRNAME=filterProperites.read("mkdirname");
-	
+	private static final String APP4SERVERIP = filterProperites
+			.read("aap4ServerIp");
+	private static final String VITUALPAGEURI = filterProperites
+			.read("vitualPageUri");
+	private static final String MKDIRNAME = filterProperites.read("mkdirname");
+
 	private URL url;
 	private HttpURLConnection connection = null;
 
@@ -37,13 +39,13 @@ public class VirtualBrowserConnection {
 
 			connection = (HttpURLConnection) url.openConnection();
 
-			//"X-Requested-With" 
+			// "X-Requested-With"
 			if (req.getHeader("X-Requested-With") == null) {
 				connection.setRequestMethod("POST");
 			} else {
 				connection.setRequestMethod("PUT");
 			}
-			
+
 			System.out
 					.println("-------------VirtualBrowserConnection ReqHeader---------------------");
 			for (Enumeration<?> e = req.getHeaderNames(); e.hasMoreElements();) {
@@ -55,19 +57,24 @@ public class VirtualBrowserConnection {
 					.println("-------------VirtualBrowserConnection ReqHeaderEND--------------------");
 			System.out.println("req.getURI:" + req.getRequestURI());
 			String temp = "";
-			///home/master/nodejs/aap4web/routes/site
+			// /home/master/nodejs/aap4web/routes/site
 			if (req.getRequestURI().equals("/index.do")) {
 				temp = "/board/index.jsp";
-				this.makeDir(MKDIRNAME+temp);
+				this.makeDir(MKDIRNAME + temp);
 				connection.setRequestProperty(VITUALPAGEURI, temp);
 			} else if (req.getRequestURI().equals("/notice_list.do")) {
 				temp = "/board/test_list.jsp";
-				this.makeDir(MKDIRNAME+temp);
+				this.makeDir(MKDIRNAME + temp);
 				connection.setRequestProperty(VITUALPAGEURI, temp);
 			} else if (req.getRequestURI().equals("/notice_content.do")) {
 				temp = "/board/test_read.jsp";
-				this.makeDir(MKDIRNAME+temp);
+				this.makeDir(MKDIRNAME + temp);
 				connection.setRequestProperty(VITUALPAGEURI, temp);
+			} else if (req.getRequestURI().equals("/notice_returnpage.do")) {
+				temp = "/board/test_list.jsp";
+				this.makeDir(MKDIRNAME + temp);
+				connection.setRequestProperty(VITUALPAGEURI, temp);
+
 			} else {
 
 				connection.setRequestProperty(VITUALPAGEURI,
@@ -126,13 +133,13 @@ public class VirtualBrowserConnection {
 				+ responseData.toString());
 
 	}
-	
-	//mkdir
-	
-	public void makeDir(String fileName){
-		File dir = new File(fileName); // 
-		if(!dir.exists()){ 
-			if(!dir.mkdirs()){
+
+	// mkdir
+
+	public void makeDir(String fileName) {
+		File dir = new File(fileName); //
+		if (!dir.exists()) {
+			if (!dir.mkdirs()) {
 				System.out.println("mkdir fail");
 			}
 		}
