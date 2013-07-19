0ffa8910259827085edbef432ad0ac158a65a744
echan82 <echan1020@gmail.com>
2013-07-19 13:45:43 +0900

    replace test

--- src/main/java/kr/co/adflow/testParser/TestClientModify.java
+++ src/main/java/kr/co/adflow/testParser/TestClientModify.java
@@ -2,25 +2,31 @@ package kr.co.adflow.testParser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestClientModify {
-		
-	Logger logger= LoggerFactory.getLogger(TestClientModify.class);
-	
-	public void jsoupModify(String html){
-		
+
+	Logger logger = LoggerFactory.getLogger(TestClientModify.class);
+
+	public void jsoupModify(String html) {
+
 		logger.debug(html);
-		
-		Document doc= Jsoup.parse(html);
-		
-		String result=doc.select("form").attr("action");
-		
-				
-		logger.debug("action Result:"+result);
-		
+
+		Document doc = Jsoup.parse(html);
+
+		if (doc.select("form").attr("action").equals("/notice_search.do")) {
+			logger.debug("Notice_Search DO Select");
+
+			String result = doc.select("form").attr("action");
+			logger.debug("result:" + result);
+
+			result = doc.select("form").attr("action")
+					.replaceAll("/notice_search.do", "");
+			logger.debug("replaceResult:" + result);
+
+		}
+
 	}
 
 }
