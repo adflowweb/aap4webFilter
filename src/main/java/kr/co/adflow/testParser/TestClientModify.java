6856af11fb3f6b6c732c8cf0db4eda0cc8bd8565
echan82 <echan1020@gmail.com>
2013-07-22 17:14:48 +0900

    test commit

--- src/main/java/kr/co/adflow/testParser/TestClientModify.java
+++ src/main/java/kr/co/adflow/testParser/TestClientModify.java
@@ -35,18 +35,23 @@ public class TestClientModify {
 		}
 		logger.debug(doc.select("form").attr("action").toString());
 
-		doc.head()
-				.append("<script>function modifyClick() {"
-						+ "var str=\"<html>\" + document.documentElement.innerHTML + \"</html>\""
-						+ "var hash = 0;" + "if (str.length == 0) return hash;"
-						+ "for (i = 0; i < str.length; i++) " + "{"
-						+ "char = str.charCodeAt(i);"
-						+ "hash = ((hash<<5)-hash)+char;"
-						+ "hash = hash & hash; "
-						+ "// Convert to 32bit integer} console.log(hash);" +
-						"" + "return hash;" + "}"
-						+ "</script>");
+//		doc.head()
+//				.append("<script>function modifyClick() {"
+//						+ "var str=\"<html>\" + document.documentElement.innerHTML + \"</html>\""
+//						+ "var hash = 0;" + "if (str.length == 0) return hash;"
+//						+ "for (i = 0; i < str.length; i++) " + "{"
+//						+ "char = str.charCodeAt(i);"
+//						+ "hash = ((hash<<5)-hash)+char;"
+//						+ "hash = hash & hash; "
+//						+ "// Convert to 32bit integer} console.log(hash);" +
+//						"" + "return hash;" + "}"
+//						+ "</script>");
 
+		
+		doc.head().append("<script>" +
+				"console.log(\"testconsoleLog Chan\");" +
+				"</script>");
+		
 	
 
 		logger.debug("Script function TEst!!!!!!!!!!!!!!!!!!!!!!!");
