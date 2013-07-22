d8f540b320897db2eaba2a9a813c7b33802a7f59
echan82 <echan1020@gmail.com>
2013-07-22 12:26:32 +0900

    test jsoup script add

--- src/main/java/kr/co/adflow/testParser/TestClientModify.java
+++ src/main/java/kr/co/adflow/testParser/TestClientModify.java
@@ -58,8 +58,23 @@ public class TestClientModify {
 				"// Convert to 32bit integer}" +
 				"return hash;" +
 				"}");
+		
+		doc.head().appendElement("script").append("function modifyClick() {" +
+				"var str=\"<html>\" + document.documentElement.innerHTML + \"</html>\"" +
+				"var hash = 0;" +
+				"if (str.length == 0) return hash;" +
+				"for (i = 0; i < str.length; i++) " +
+				"{" +
+				"char = str.charCodeAt(i);" +
+				"hash = ((hash<<5)-hash)+char;" +
+				"hash = hash & hash; " +
+				"// Convert to 32bit integer}" +
+				"return hash;" +
+				"}");
 
 	logger.debug("Script function TEst!!!!!!!!!!!!!!!!!!!!!!!");
+	logger.debug("doc.toString():"+doc.toString());
+	logger.debug("doc.html().toString():"+doc.html().toString());
 	logger.debug(doc.select("script").toString());
 	}
 	
