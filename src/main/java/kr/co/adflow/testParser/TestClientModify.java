277376b681aa22b70a7e338d6105bb647a33802b
echan82 <echan1020@gmail.com>
2013-07-23 16:19:03 +0900

    test append

--- src/main/java/kr/co/adflow/testParser/TestClientModify.java
+++ src/main/java/kr/co/adflow/testParser/TestClientModify.java
@@ -43,11 +43,11 @@ public class TestClientModify {
 		// + "</script>");
 	
 		
-		doc.head()
+		/*doc.head()
 				.append("<script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js\"></script>");
 		doc.head()
 				.append("<script type=\"text/javascript\"src=\"https://hashmask.googlecode.com/svn-history/r2/trunk/jquery.sha1.js\"></script>");
-
+*/
 		doc.head()
 				.append("<script>"
 						+ "function lengthInUtf8Bytes(str) {"
