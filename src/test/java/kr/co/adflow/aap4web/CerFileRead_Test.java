e9a2f039401d0539d1b71bd3d0dcbb230756fb50
echan82 <echan1020@gmail.com>
2013-11-12 12:17:40 +0900

    CerFileRead

---
+++ src/test/java/kr/co/adflow/aap4web/CerFileRead_Test.java
@@ -0,0 +1,33 @@
+package kr.co.adflow.aap4web;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.PublicKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+
+import org.junit.Test;
+
+public class CerFileRead_Test {
+
+	@Test
+	public void test() throws CertificateException, IOException {
+		FileInputStream fis = new FileInputStream("c:\\adf.cer");
+		BufferedInputStream bis = new BufferedInputStream(fis);
+
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+		Certificate cert = cf.generateCertificate(bis);
+
+		PublicKey publicKey = cert.getPublicKey();
+
+		byte[] publicKeyByte = publicKey.getEncoded();
+
+		String publicKeyStr = new java.math.BigInteger(publicKeyByte)
+				.toString(16);
+		System.out.println("publicKeyStr:" + publicKeyStr);
+
+	}
+}
